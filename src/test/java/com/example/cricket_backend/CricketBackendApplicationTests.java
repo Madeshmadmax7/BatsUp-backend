d2f5a2fd8f64d9c94a74417d2996e18d4cfa8107
MADMAX <amadesh702@gmail.com>
2025-08-24 22:23:34 +0530

    commit

--- src/test/java/com/example/cricket_backend/CricketBackendApplicationTests.java
+++ src/test/java/com/example/cricket_backend/CricketBackendApplicationTests.java
@@ -1,9 +1,11 @@
 package com.example.cricket_backend;
 
+import org.springframework.test.context.ActiveProfiles;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CricketBackendApplicationTests {
 
 	@Test
